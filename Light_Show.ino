f708ac0ac5afe5755aab7785665120bb2612f2d1
XDeric <eeeric3995@yahoo.com>
2016-05-11 23:47:14 -0400

    Update Light_Show.ino

--- Light_Show.ino
+++ Light_Show.ino
@@ -1,4 +1,4 @@
-// Eric Mei Light Project
+// Eric Mei Light Show
 
 #include <Adafruit_GFX.h>   //AdaFruit Core graphics library
 #include <RGBmatrixPanel.h> //AdaFruit Hardware-specific library
@@ -21,18 +21,22 @@
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
+//random variables
+long random_n, random_n1, random_n2;
+
 //ping sensor variables
 long unsigned int duration = 0, inches = 0, cm = 0;
 
 void setup() {
  Serial.begin(115200); //Communication Speed for monitor
- 
+
  //starts the sensor
- pinMode(TRIG, OUTPUT); /* Trigger Pin */
- pinMode(ECHO, INPUT); /* Echo pin */
+ pinMode(TRIG, OUTPUT); // Trigger Pin 
+ pinMode(ECHO, INPUT);  // Echo pin 
  
+ randomSeed(analogRead(5));// analog pin not in use, it's to help start with a random number 
  // Setting time
- setTime(7,1,0,10,5,16);  //hours, minutes, seconds, days, months, years
+ setTime(8,45,0,12,5,16);  //hours, minutes, seconds, days, months, years
  
  matrix.begin();
  matrix.drawPixel(0, 0, matrix.Color333(7, 7, 7)); // First led is white
@@ -73,40 +77,162 @@ void loop()
  delay(10 * Pause); // Pause before next pulse
  
  //light Show 
- if (cm <= 5) // display light show 1 if object less than 5 cm away
+ if (cm <= 5) // display light show 1 if object less than 5 inches away
   {
     Serial.print("5 cm away");
     matrix.setTextColor(matrix.Color333(7,0,0));
     matrix.setCursor(1, 0); // start at top left, with one pixel of spacing
     matrix.print("5 cm");
     delay(1000); 
-    matrix.fillRect(0, 10, 32, 32, matrix.Color333(0, 0, 0)); //black
+    matrix.setTextColor(matrix.Color333(7,0,0));
+    matrix.setCursor(1, 0); // start at top left, with one pixel of spacing
+    matrix.print("5 cm");
+    delay(100); 
+    
+    //Walking Stick_Man phase 1
+    
+    matrix.fillCircle(5, 10, 3, matrix.Color333(1, 1, 1)); //white Head
+    //
+    matrix.drawLine(5, 13, 5, 20, matrix.Color333(1, 1, 1));//body
+    //
+    matrix.drawLine(5, 14, 2, 16, matrix.Color333(1, 1, 1));//R_arm
+    //
+    matrix.drawLine(2, 16, 2, 20, matrix.Color333(1, 1, 1));//R_arm
+    //
+    matrix.drawLine(5, 13, 6, 16, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(6, 16, 9, 19, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(5, 21, 4, 23, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(4, 23, 3, 29, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(5, 21, 7, 23, matrix.Color333(1, 1, 1));//L_leg
+    //
+    matrix.drawLine(7, 23, 7, 29, matrix.Color333(1, 1, 1));//L_leg
     delay(100);
-    matrix.fillRect(0, 10, 32, 32, matrix.Color333(1, 0, 0)); //Red
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0)); //clear
+    
+    //Walking Stick_Man phase 2
+    
+    matrix.fillCircle(5, 10, 3, matrix.Color333(1, 1, 1)); //white Head
+    //
+    matrix.drawLine(5, 13, 5, 20, matrix.Color333(1, 1, 1));//body
+    //
+    matrix.drawLine(5, 15, 3, 15, matrix.Color333(1, 1, 1));//R_arm
+    //
+    matrix.drawLine(3, 15, 6, 20, matrix.Color333(1, 1, 1));//R_arm
+    matrix.drawPixel(4, 16, matrix.Color333(0, 0, 0)); //Edit
+    matrix.drawPixel(3, 16, matrix.Color333(1, 1, 1)); //Edit
+    matrix.drawPixel(3, 15, matrix.Color333(0, 0, 0)); //Edit
+    //
+    matrix.drawLine(5, 14, 6, 16, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(6, 16, 8, 20, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(5, 21, 7, 23, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(7, 23, 2, 28, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(5, 21, 5, 29, matrix.Color333(1, 1, 1));//L_leg
+    //
     delay(100);
-    matrix.fillRect(0, 10, 32, 32, matrix.Color333(1, 1, 0)); //Yellow
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0)); //clear
+    
+    //Walking Stick_Man phase 3
+    
+    matrix.fillCircle(5, 10, 3, matrix.Color333(1, 1, 1)); //white Head
+    //
+    matrix.drawLine(5, 13, 5, 20, matrix.Color333(1, 1, 1));//body
+    //
+    matrix.drawLine(5, 15, 3, 15, matrix.Color333(1, 1, 1));//R_arm
+    matrix.drawPixel(3, 15, matrix.Color333(0, 0, 0)); //Edit
+    //
+    matrix.drawLine(3, 16, 3, 20, matrix.Color333(1, 1, 1));//R_arm
+    //
+    matrix.drawLine(5, 13, 6, 16, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(6, 16, 8, 19, matrix.Color333(1, 1, 1));//L_arm
+    //
+    matrix.drawLine(5, 21, 4, 23, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(4, 23, 3, 28, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(5, 21, 6, 23, matrix.Color333(1, 1, 1));//L_leg
+    //
+    matrix.drawLine(7, 24, 4, 28, matrix.Color333(1, 1, 1));//L_leg
     delay(100);
-    matrix.fillRect(0, 10, 32, 32, matrix.Color333(0, 0, 1)); //Blue
+    matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0)); //clear
+    
+    //Walking Stick_Man phase 4
+    
+    matrix.fillCircle(5, 10, 3, matrix.Color333(1, 1, 1)); //white Head
+    //
+    matrix.drawLine(5, 13, 5, 20, matrix.Color333(1, 1, 1));//body
+    //
+    matrix.drawLine(4, 16, 4, 19, matrix.Color333(1, 1, 1));//R_arm
+    //
+    matrix.drawLine(5, 15, 7, 20, matrix.Color333(1, 1, 1));//L_arm
+    matrix.drawPixel(7, 19, matrix.Color333(0, 0, 0)); //Edit
+    //
+    matrix.drawLine(5, 21, 4, 23, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(4, 23, 4, 29, matrix.Color333(1, 1, 1));//R_leg
+    //
+    matrix.drawLine(5, 21, 7, 23, matrix.Color333(1, 1, 1));//L_leg
+    //
+    matrix.drawLine(7, 23, 7, 27, matrix.Color333(1, 1, 1));//L_leg
     delay(100);
     matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0)); //clear
     delay(100);
   }
-  else if ( cm > 5 && cm < 15)
+  
+  else if ( cm > 5 && cm < 15) // circle light show for 5-15 inches away
     {
     matrix.setTextColor(matrix.Color333(7,0,0));
     matrix.setCursor(1, 0); // start at top left, with one pixel of spacing
     matrix.print("10 cm");
     delay(1000);
-    matrix.fillCircle(16, 18, 10, matrix.Color333(1, 0, 1)); //Purple
-    delay(100);
-    matrix.fillCircle(16, 18, 10, matrix.Color333(0, 1, 0)); //Green
-    delay(100);
-    matrix.fillCircle(16, 18, 10, matrix.Color333(7, 1, 0)); //Orange
-    delay(100);
+    random_n = random(7); // to generate random number 0-7 color
+    random_n1 = random(7); // to generate random number 0-7 color
+    random_n2 = random(7); // to generate random number 0-7 color
+    //circle in middle
+    matrix.fillCircle(16, 18, 8, matrix.Color333(random_n, random_n1, random_n2));
+    delay(200);
+    matrix.fillCircle(16, 18, 8, matrix.Color333(random_n1, random_n, random_n2));
+    delay(200);
+    matrix.fillCircle(16, 18, 8, matrix.Color333(random_n2, random_n1, random_n));
+    delay(200);
+    // circle around
+    matrix.fillCircle(5, 15, 2, matrix.Color333(random_n, random_n2, random_n1));
+    delay(200);
+    matrix.fillCircle(5, 15, 2, matrix.Color333(0, 0, 0));
+    delay(200);
+    matrix.fillCircle(6, 22, 2, matrix.Color333(random_n1, 1, 1));
+    delay(200);
+    matrix.fillCircle(6, 22, 2, matrix.Color333(0, 0, 0));
+    delay(200);
+    matrix.fillCircle(16, 29, 2, matrix.Color333(0, 1, random_n2));
+    delay(200);
+    matrix.fillCircle(16, 29, 2, matrix.Color333(0, 0, 0));
+    delay(200);
+    matrix.fillCircle(26, 22, 2, matrix.Color333(random_n, 1, 1));
+    delay(200);
+    matrix.fillCircle(26, 22, 2, matrix.Color333(0, 0, 0));
+    delay(200);
+    matrix.fillCircle(27, 15, 2, matrix.Color333(0, random_n1, 1));
+    delay(200);
+    matrix.fillCircle(27, 15, 2, matrix.Color333(0, 0, 0));
+    delay(200);
+    matrix.fillCircle(16, 7, 2, matrix.Color333(random_n1, 0, random_n2));
+    delay(200);
+    matrix.fillCircle(16, 7, 2, matrix.Color333(0, 0, 0));
+    delay(200);
     matrix.fillRect(0, 0, 32, 32, matrix.Color333(0, 0, 0)); //clear
-    delay(100);
+    delay(200);
     }
-  else{
+  
+  else{ // returns to telling time
     //Displays in Serial Monitor
     Serial.print(month()); // this display the month/day/year format
     Serial.print("/");
@@ -140,3 +266,4 @@ void loop()
     matrix.fillScreen(matrix.Color333(0, 0, 0)); //fill screen black*/
   }
  }
+
